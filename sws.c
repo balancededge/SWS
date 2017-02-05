50019608d42b7d9b27c02928ecf64075c7e6b317
Eric Buss <ebuss@ualberta.ca>
2017-02-05 15:04:53 -0800

    upgrade

--- sws.c
+++ sws.c
@@ -524,10 +524,8 @@ int FILE_in_directory(const char* path) {
     printf("real path: %s\n", full_path);
     printf("is file: %d\n", FILE_is_file(full_path));
     printf("in dir: %s -> %d\n", CNFG_directory, strncmp(CNFG_directory, full_path, strlen(CNFG_directory)));
-    printf("all: %d\n", (strncmp(CNFG_directory, full_path, strlen(CNFG_directory)) == 0) &&
-        FILE_is_file(path));
     return
-        (strncmp(CNFG_directory, full_path, strlen(CNFG_directory)) == 0) &&
+        !strncmp(CNFG_directory, full_path, strlen(CNFG_directory) &&
         FILE_is_file(path);
 }
 
