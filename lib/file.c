cbf56b0921c4eee9ecad8524a089b0e863798d35
Eric Buss <ebuss@ualberta.ca>
2017-02-08 12:24:10 -0800

    upgrade

--- lib/file.c
+++ lib/file.c
@@ -90,7 +90,7 @@ int read_file(char* pointer, const int n, const char* path) {
     if(file) {
         fseek(file, 0, SEEK_END);
         file_size = ftell(file);
-        pointer = (char*) malloc(sizeof(char) * file_size + 1);
+        pointer = (char*) malloc(sizeof(char) * (file_size + 1));
         fseek(file, 0, SEEK_SET);
         pointer[0] = 0;
         //fread(pointer, 1, file_size, file);
